4778dbc4297e5ceb3632b4988efde06665684c66
Milo1004 <82019161+milo1004@users.noreply.github.com>
2024-04-29 21:25:19 +0800

    Update main.py
    DOS is no longer in the computer.
    The command 'edos' has been deleted permanently.
    The file 'main.py' has been converted to an exe file.
    Added the command 'time'

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import random
 from datetime import datetime
 command = (0)
 def start():
+  os.system('cls')
   print("booting RAM...")
   time.sleep(3)
   print("booting CPU...")
@@ -28,23 +29,24 @@ def start():
   print("loading properties")
   time.sleep(3)
   os.system("cls")
-  print("Welcome to PythonOS 1.0!")
+  print("Welcome to PythonOS 1.1")
   time.sleep(0.5)
   print(
-      "You may begin entering the commands. Type 'help' for a list of commands. Or, you may type'apps' to show a list of apps."
+      "You may begin entering the commands. Type 'help' for a list of commands. Or, you may type'apps' to show a list of apps.(Update Log added to PythonOS. Type 'update log' to check the things that are updated.)"
   )
   while True:
     command = input("C:\command> ")
     if command == "help":
       print("commands:")
-      print("help - shows this list")
-      print("apps - shows a list of apps")
-      print("shutdown - shuts down the computer")
-      print("pyver - shows the current version of PythonOS")
-      print("date - shows the current date")
-      print("edos - exit to dos")
-      print("restart - restarts the computer")
+      print("help - shows this list.")
+      print("apps - shows a list of apps.")
+      print("shutdown - shuts down the computer.")
+      print("pyver - shows the current version of PythonOS.")
+      print("date - shows the current date.")
+      print("time - shows the current time.")
+      print("restart - restarts the computer.")
       print("cls  /  clear - clear the terminal(you can still use the computer normally.)")
+      print("update log - check the update log through this command.")
       print("Note: If you want to open an app, type the app name, and add '.exe' behind.")
     elif command == "apps":
       print("apps:")
@@ -67,36 +69,23 @@ def start():
         print("shutdown cancelled")
         time.sleep(1)
     elif command == "pyver":
-      print("PythonOS 1.0")
+      print("PythonOS 1.1")
       print("Copyright © 2024, python.org")
       print("All rights reserved.")
       print("This is a free operating system. It is not endorsed by python.org")
-      print("you can download this software from github.com")
+      print("you can download this software from https://github.com/milo1004/PythonOS.git")
       print("(end of properties)")
     elif command == "date":
       datenow = datetime.now()
-      current_date = datenow.strftime("%Y-%m-%d")
+      print("The date will be shown like this:YYYY/MM/DD")
+      current_date = datenow.strftime("%Y/%m/%d")
       print(f"The Current date is : {current_date}")
-    elif command == "edos":
-      edos = input("Are you sure you want to exit to MS-DOS? y/n>")
-      if edos == "y":
-        print("See you next time!")
-        time.sleep(1)
-        os.system("cls")
-        sys.exit()
-      elif edos == "n":
-        print("Exit cancelled.")
-        time.sleep(1)
-      else:
-        print("Invalid input.")
-        time.sleep(1)
-        print("Exit cancelled.")
-        time.sleep(1)
-    elif command == "":
-      print("Invalid input.")
-      time.sleep(1)
+    elif command == "time":
+      timenow = datetime.now()
+      print("The time will be shown like this:HH:MM")
+      current_time = timenow.strftime("%H:%M")
+      print(f"The Current time is : {current_time}")
     elif command == "random.exe":
-      
       Lrandom = input("Enter the least number>")
       if Lrandom == "":
         print("Invalid input.")
@@ -118,7 +107,18 @@ def start():
       elif restart == "n":
         print("Restart cancelled")
         time.sleep(1)
-    elif command == "cls" or "clear":
+    elif command == "update log":
+      print("Update Log:")
+      print("DOS is no longer in the computer.")
+      print("The command 'edos' has been deleted permanently")
+      print("The file 'main.py' has been converted to an exe file.")
+      print("Added the command 'time'")
+      time.sleep(1)
+    elif command == "":
+      print("Invalid input.")
+    elif command == "cls":
+      os.system('cls')
+    elif command == "clear":
       os.system('cls')
     else:
       print("Illegal command. Type 'help' for a list of commands.")
